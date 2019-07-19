a379e0adaabf4422396e8e4764fe1ae1ff4b429d
Kyle Apostol <kyleapostol@yahoo.com>
2019-07-19 17:15:03 +0000

    unfinished

--- client/components/ProductListItem.jsx
+++ client/components/ProductListItem.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 class ProductListItem extends React.Component {
-    render(){
-        let ProductInfo = this.productInfo;
-        return (
-            <div className="card" style="width: 18rem;">
-                <img className="card-img-top" src="..." alt="Card image cap"/>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text"></p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    let ProductInfo = this.productInfo;
+    return (
+      <div className="card" style="width: 18rem;">
+        <img className="card-img-top" src="..." alt="Card image cap"/>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text"></p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ProductListItem;
