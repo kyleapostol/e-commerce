8343a304ebf4ca7d1cc9c8ab0de4b6a27f7718a7
Kyle Apostol <kyleapostol@yahoo.com>
2019-10-03 01:13:39 +0000

    finish form validation on checkout component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -14,7 +14,7 @@ class App extends React.Component {
       cart : [],
       productArr: [],
       view : {
-        name : 'landing-page',
+        name : 'checkout',
         params : {}
       }
     };
