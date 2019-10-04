922f8ccca3b039fc514a307a37ad0c9f4708f6b5
Kyle Apostol <kyleapostol@yahoo.com>
2019-10-04 22:50:28 +0000

    fixed some responsive css issues

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,52 +1,52 @@
 <?php
 
-// header('Content-Type: application/json');
+header('Content-Type: application/json');
 
-// if (empty($_GET['id'])) {
-//   readfile('dummy-products-list.json');
-// } else {
-//   readfile('dummy-product-details.json');
+if (empty($_GET['id'])) {
+  readfile('dummy-products-list.json');
+} else {
+  readfile('dummy-product-details.json');
+}
+
+// require_once('./functions.php');
+// require_once('./db_connection.php');
+
+// set_exception_handler('error_handler');
+
+// startup();
+
+// if (!$conn) {
+//   throw new Exception('exception: ' . mysqli_connect_error());
+//   exit();
+// };
+
+// if ( empty($_GET['id']) ) { //If empty, return all products
+//   $query = "SELECT * FROM `products`"; 
+// } else if (!empty( $_GET['id'] ) && !is_numeric( $_GET['id'] )) { //If not empty, and an invalid number
+//     throw new Exception("Id needs to be a number");
+// } else if (!empty( $_GET['id'] ) && is_numeric( $_GET['id'] )) { //if not empty, and valid number
+//   $id = $_GET['id'];
+//   $query = "SELECT * FROM `products` WHERE `id` = $id"; 
+// };
+
+// $result = mysqli_query($conn, $query);
+
+// $num_rows_check = mysqli_num_rows($result);
+
+// if(!$num_rows_check) { 
+//   throw new Exception("Invalid ID: ". $_GET['id']);
 // }
 
-require_once('./functions.php');
-require_once('./db_connection.php');
 
-set_exception_handler('error_handler');
+// if(!$result) {
+//   throw new Exception('exception:', $result);
+// }
 
-startup();
+// $output = [];
+// while($row = mysqli_fetch_assoc($result)) {
+//   $output[] = $row;
+// }
 
-if (!$conn) {
-  throw new Exception('exception: ' . mysqli_connect_error());
-  exit();
-};
-
-if ( empty($_GET['id']) ) { //If empty, return all products
-  $query = "SELECT * FROM `products`"; 
-} else if (!empty( $_GET['id'] ) && !is_numeric( $_GET['id'] )) { //If not empty, and an invalid number
-    throw new Exception("Id needs to be a number");
-} else if (!empty( $_GET['id'] ) && is_numeric( $_GET['id'] )) { //if not empty, and valid number
-  $id = $_GET['id'];
-  $query = "SELECT * FROM `products` WHERE `id` = $id"; 
-};
-
-$result = mysqli_query($conn, $query);
-
-$num_rows_check = mysqli_num_rows($result);
-
-if(!$num_rows_check) { 
-  throw new Exception("Invalid ID: ". $_GET['id']);
-}
-
-
-if(!$result) {
-  throw new Exception('exception:', $result);
-}
-
-$output = [];
-while($row = mysqli_fetch_assoc($result)) {
-  $output[] = $row;
-}
-
-print( json_encode($output) ); 
+// print( json_encode($output) ); 
 
 ?>
