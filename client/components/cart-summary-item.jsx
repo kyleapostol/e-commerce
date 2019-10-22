91b21bccae8cc345b291468fca26d6cc39d3a0f4
Kyle Apostol <kyleapostol@yahoo.com>
2019-10-22 04:49:00 +0000

    fixed modal issues in cart summary responsiveness

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 
-export default class CartSummaryItem extends React.Component{
-    render(){
-        let item = this.props.cartItem;
-        return (
-            <div className="cart-margin">            
-                <div className="container cart-container">
-                    <div className="row">
-                        <div className="col">
-                            <img src={ item.image } className="card-img-top card-img-top-2"/>
-                        </div>
-                        <div className="col">
-                            <ul className="mt-3">
-                                <li><b>Name:</b> { item.name }</li>
-                                <li><b>Color:</b> { item.color }</li>
-                                <li><b>Price:</b> { item.price }</li>
-                                <li><b>Qyt:</b> {item.count}</li>
-                                <button type="button" className="btn btn-danger btn-sm mt-5">Delete</button>
-                            </ul> 
-                        </div>
-                    </div>
-                </div>
+export default class CartSummaryItem extends React.Component {
+  render() {
+    let item = this.props.cartItem;
+    return (
+      <div className="cart-margin">
+        <div className="container cart-container">
+          <div className="row">
+            <div className="col">
+              <img src={ item.image } className="card-img-top card-img-top-2"/>
             </div>
+            <div className="col">
+              <ul className="mt-3">
+                <li><b>Name:</b> { item.name }</li>
+                <li><b>Color:</b> { item.color }</li>
+                <li><b>Price:</b> { item.price }</li>
+                <li><b>Qyt:</b> {item.count}</li>
+                <button type="button" className="btn btn-danger btn-sm mt-5 delete-margin">Delete</button>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
 
-        )
+    );
 
-    }
+  }
 }
